01dbe0081b1f529f7562ffa34e56988be9a73d88
sagar <saagar.rachakonda@gmail.com>
2024-09-25 12:03:30 +0530

    Implemented yml file, Profiles, Actuator endpoints

--- src/main/java/com/datadesign/datadesign/controller/DataController.java
+++ src/main/java/com/datadesign/datadesign/controller/DataController.java
@@ -1,5 +1,6 @@
 package com.datadesign.datadesign.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DataController {
 
+    @Value("${welcomemsg.config}")
+    private String WelcomeMessage;
     //@RequestMapping(value = "/", method = RequestMethod.GET)
     @GetMapping("/")
     public String DataDetails(){
-        return "Welcome to Data Centre Sagar";
+        return WelcomeMessage;
     }
 }
